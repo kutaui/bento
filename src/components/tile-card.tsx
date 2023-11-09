88df6da11f8ba64667b247465c3ceb35bf8d31d0
kutaui <kutaybekleric@gmail.com>
2023-11-10 01:32:42 +0300

    first init

---
+++ src/components/tile-card.tsx
@@ -0,0 +1,31 @@
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle
+} from '@/components/ui/card';
+import Image from 'next/image';
+import { cn } from "@/lib/utils"
+
+
+type TileCardProps = {
+    title?: string;
+    description?: string;
+    href?: string;
+    image?: string;
+    background?: string;
+};
+
+export const TileCard = ({ title = "more soon", description = "", href = "/" , image= "/loading.png", background= ""}: TileCardProps) => {
+    return <a href={href} target="_blank" className="group">
+        <Card className={cn("w-64 bg-zinc-900 transition rounded-3xl h-36 overflow-hidden hover:scale-[1.02] hover:border-[3px] hover:cursor-pointer bg-cover",background)} >
+            <CardHeader className="relative pt-10">
+                <CardTitle className="text-white translate-y-6 group-hover:translate-y-0 transition text-xl font-semibold">
+                    {image && <Image src={image} alt="asd" width={25} height={20} className="pb-2"/>}
+                    {title}
+                </CardTitle>
+                <CardDescription className="text-white font-medium translate-y-6 group-hover:translate-y-0 z-10 opacity-0 group-hover:opacity-100 transition">{description}</CardDescription>
+            </CardHeader>
+        </Card>
+    </a>;
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,14 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ['class'],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}'
+    ],
+    theme: {
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [require('tailwindcss-animate')]
+};
