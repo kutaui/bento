74f8abc016c2085d1ece25e80345e2b966692df8
kutaui <kutaybekleric@gmail.com>
2023-11-10 16:41:53 +0300

    change twitter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home(this: any) {
         <section className="flex flex-wrap gap-4 animate-fade-left animate-ease-in-out w-full">
             <TileCard href="https://github.com/kutaui" title="github/kutaui" description="my code" image="/github-mark-white.png" background="bg-[url('/github-graph.png')]"/>
             <TileCard href="https://medium.com/@kutaui" title="medium/kutaui" description="stuff i wrote" image="/medium.png" background="bg-[url('/bubble.png')] bg-contain bg-right bg-no-repeat"/>
-            <TileCard href="https://twitter.com/kutaybekleric" title="twitter" description="yeah i know" image="/twitter.png" background="bg-[url('/twitter-bg.jpg')] bg-no-repeat" />
+            <TileCard href="https://twitter.com/kutaul" title="twitter" description="yeah i know" image="/twitter.png" background="bg-[url('/twitter-bg.jpg')] bg-no-repeat" />
             <TileCard/>
             <TileCard/>
             <TileCard/>
