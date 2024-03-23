0c770be4a5f4d1127c5d5c80aa4c6191f104e4d1
kutaui <kutaybekleric@gmail.com>
2024-03-23 17:50:29 +0300

    remove linkedin add blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ export default function Home(this: any) {
     return <main className="max-w-[1180px] mx-auto px-10 py-36">
         <section className="flex flex-wrap gap-4 animate-fade-left animate-ease-in-out w-full">
             <TileCard href="https://github.com/kutaui" title="github/kutaui" description="my code" image="/github-mark-white.png" background="bg-[url('/github-graph.png')]"/>
-            <TileCard href="https://medium.com/@kutaui" title="medium/kutaui" description="stuff i wrote" image="/medium.png" background="bg-[url('/bubble.png')] bg-contain bg-right bg-no-repeat"/>
-            <TileCard href="https://www.linkedin.com/in/kutaybekleric/" title="linkedin" description="professional" image="/linkedin.png" background="bg-[url('/linkedin-bg.jpg')] bg-[0px_230px] pt-1" />
+            <TileCard href="https://blog.kutaybekleric.com" title="blog" description="stuff i wrote" image="/medium.png" background="bg-[url('/bubble.png')] bg-contain bg-right bg-no-repeat"/>
             <TileCard/>
             <TileCard/>
             <TileCard/>
