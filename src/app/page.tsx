cf581ec4f6e67015e36c88a29d116e7a1614bc03
kutaui <kutaybekleric@gmail.com>
2024-04-09 16:26:32 +0300

    feat: seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
-import { TileCard } from '@/components/tile-card';
+import { TileCard } from '@/components/tile-card'
 
 export default function Home(this: any) {
-    return <main className="max-w-[1180px] mx-auto px-10 py-36">
-        <section className="flex flex-wrap gap-4 animate-fade-left animate-ease-in-out w-full">
-            <TileCard href="https://github.com/kutaui" title="github/kutaui" description="my code" image="/github-mark-white.png" background="bg-[url('/github-graph.png')]"/>
-            <TileCard href="https://blog.kutaybekleric.com" title="blog" description="stuff i wrote" image="/medium.png" background="bg-[url('/bubble.png')] bg-contain bg-right bg-no-repeat"/>
-            <TileCard/>
-            <TileCard/>
-            <TileCard/>
-            <TileCard/>
-            <TileCard/>
-            <TileCard/>
-            <TileCard/>
-            <TileCard/>
-            <TileCard/>
-        </section>
-        <section>
-
-        </section>
-    </main>;
-
+	return (
+		<main className="max-w-[1180px] mx-auto px-10 py-36">
+			<section className="flex flex-wrap gap-4 animate-fade-left animate-ease-in-out w-full">
+				<TileCard
+					href="https://github.com/kutaui"
+					title="github/kutaui"
+					description="my code"
+					image="/github-mark-white.png"
+					background="bg-[url('/github-graph.png')]"
+				/>
+				<TileCard
+					href="https://blog.kutaybekleric.com"
+					title="blog"
+					description="stuff i wrote"
+					image="/medium.png"
+					background="bg-[url('/bubble.png')] bg-contain bg-right bg-no-repeat"
+				/>
+				<TileCard
+					href="https://kutaybekleric.com"
+					title="my"
+					description="projects"
+					image="false"
+					background="bg-[url('/portfolio.png')] bg-contain bg-center bg-no-repeat"
+				/>
+				<TileCard />
+				<TileCard />
+				<TileCard />
+				<TileCard />
+				<TileCard />
+				<TileCard />
+				<TileCard />
+				<TileCard />
+				<TileCard />
+			</section>
+		</main>
+	)
 }
--- src/components/tile-card.tsx
+++ src/components/tile-card.tsx
@@ -1,42 +1,58 @@
 import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle
-} from '@/components/ui/card';
-import Image from 'next/image';
-import { cn } from '@/lib/utils';
-
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+import { Divide } from 'lucide-react'
 
 type TileCardProps = {
-    title?: string;
-    description?: string;
-    href?: string;
-    image?: string;
-    background?: string;
-};
+	title?: string
+	description?: string
+	href?: string
+	image?: string
+	background?: string
+}
 
 export const TileCard = ({
-                             title = 'more',
-                             description = 'soon',
-                             href = '/',
-                             image = '/loading.png',
-                             background = ''
-                         }: TileCardProps) => {
-    return <a href={href} target="_blank" className="group">
-        <Card
-            className={cn('w-64 hover:animate-wiggle animate-once animate-ease-in-out' +
-                ' bg-zinc-900 transition rounded-3xl h-36' +
-                ' overflow-hidden hover:scale-[1.02] hover:border-[3px] hover:cursor-pointer bg-cover', background)}>
-            <CardHeader className="relative pt-10">
-                <CardTitle
-                    className="text-white translate-y-6 group-hover:translate-y-0 transition text-lg font-semibold ">
-                    {image && <Image src={image} alt="asd" width={25} height={20} className="pb-2"/>}
-                    {title}
-                </CardTitle>
-                <CardDescription
-                    className="text-white font-medium translate-y-6 group-hover:translate-y-0 z-10 opacity-0 group-hover:opacity-100 transition">{description}</CardDescription>
-            </CardHeader>
-        </Card>
-    </a>;
-};
+	title = 'more',
+	description = 'soon',
+	href = '/',
+	image = '/loading.png',
+	background = '',
+}: TileCardProps) => {
+	return (
+		<a href={href} target="_blank" className="group">
+			<Card
+				className={cn(
+					'w-64 hover:animate-wiggle animate-once animate-ease-in-out' +
+						' bg-zinc-900 transition rounded-3xl h-36' +
+						' overflow-hidden hover:scale-[1.02] hover:border-[3px] hover:cursor-pointer bg-cover',
+					background
+				)}
+			>
+				<CardHeader className="relative pt-10">
+					<CardTitle className="text-white translate-y-6 group-hover:translate-y-0 transition text-lg font-semibold ">
+						{image !== 'false' ? (
+							<Image
+								src={image}
+								alt="asd"
+								width={25}
+								height={20}
+								className="pb-2"
+							/>
+						) : (
+							<div className="w-[25px] h-[33px] pb-2" />
+						)}
+						{title}
+					</CardTitle>
+					<CardDescription className="text-white font-medium translate-y-6 group-hover:translate-y-0 z-10 opacity-0 group-hover:opacity-100 transition">
+						{description}
+					</CardDescription>
+				</CardHeader>
+			</Card>
+		</a>
+	)
+}
